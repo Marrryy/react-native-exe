a8a6105a68f0c175c6a48e41bc48c0116cb16673
Marrryy <ling.310500@gmail.com>
2019-07-25 11:11:56 +0700

    component part 1

---
+++ components/MenuComponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import pic from './images/uthappizza.png';
+
+function Menu(props){
+    const renderMenuItem = ({item, index}) =>{
+        return(
+            <ListItem
+                key={index}
+                title={item.name}
+                subtitle={item.description}
+                hideChevron ={true} //right arrow for ios is now hide
+                leftAvatar={{ source: {pic}}}
+            />
+        );
+    }
+    return(
+      <FlatList
+        data={props.dishes}
+        renderItem={renderMenuItem}
+        keyExtractor={item =>item.id.toString()}
+      />
+    );
+} 
+
+export default Menu;
