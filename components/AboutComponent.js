60af2a893dc2b1c1702fdf90fe08ae1c00fbc62f
Marrryy <ling.310500@gmail.com>
2019-07-30 12:17:39 +0700

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Card, ListItem } from 'react-native-elements';
 import { ScrollView,Text,FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
   return {
@@ -38,23 +39,41 @@ class About extends Component{
         />
       );
     }; 
-    return(
-      <ScrollView style={{flex:1}}>
+    if(this.props.leaders.isLoading){
+      return(
+        <ScrollView>
+          <History/>
+          <Card title='Corporate Leadership'>
+            <Loading/>
+          </Card>
+        </ScrollView>
+      );
+    }else if(this.props.leaders.errMess){
+      return(
+      <ScrollView>
         <History/>
-        <Card
-          title="Coorporate Leadership">
-            <FlatList
-              data={this.props.leaders.leaders}
-              renderItem={renderLead}
-              keyExtractor={item =>item.id.toString()}
-            />
+        <Card title='Corporate Leadership'>
+          <Text>{this.props.leaders.errMess}</Text>
         </Card>
-
-
       </ScrollView>
-    );
+      );
+      
+    }else{
+      return(
+        <ScrollView style={{flex:1}}>
+          <History/>
+          <Card
+            title="Coorporate Leadership">
+              <FlatList
+                data={this.props.leaders.leaders}
+                renderItem={renderLead}
+                keyExtractor={item =>item.id.toString()}
+              />
+          </Card>  
+        </ScrollView>
+      );
+    }
   }
-
 }
 
 export default connect(mapStateToProps)(About);
